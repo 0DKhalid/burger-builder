b862f234b09c0c27dfcd52f10d0b020910c4603f
khalid <khalid@dev.com>
2019-03-26 06:44:30 +0300

    Initial commit from Create React App

---
+++ src/container/BurgerBuilder.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import classes from './BurgerBuilder.css'
+import Aux from '../hoc/Aux';
+
+import Burger from '../components/Burger/Burger';
+
+
+
+class BurgerBuilder extends Component {
+
+    render(){
+        return (
+            <Aux>
+                <Burger />
+                <div>Build Control</div>
+            </Aux>
+        )
+    }
+} 
+
+
+export default BurgerBuilder;
